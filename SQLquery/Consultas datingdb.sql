e93e80e2f9c378e82a37ee000dd2920b11d56033
Richard <masacregalvan@gmail.com>
2023-02-27 17:43:36 -0600

    Repositorio la Seccion 2 del curso

---
+++ SQLquery/Consultas datingdb.sql
@@ -0,0 +1,13 @@
+INSERT into Users (Id, UserName)
+VALUES (1, 'Ricardo');
+
+INSERT into Users (Id, UserName)
+VALUES (2, 'Rodrigo');
+
+INSERT into Users (Id, UserName)
+VALUES (3, 'Jessica');
+
+INSERT into Users (Id, UserName)
+VALUES (4, 'Gabriela');
+
+SELECT * from Users;
